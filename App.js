639a5da9ad79527f534879d60683212fa52639dc
aryan1234-coder <aryansachan2017@gmail.com>
2024-04-27 21:39:37 +0530

    Session 01

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading=React.createElement("h1",{id: "heading"},"Hello from React");
     // This is one creating h1 inside react
     // now if I want to put into my browsser we will use rect dom
@@ -8,17 +11,31 @@ const heading=React.createElement("h1",{id: "heading"},"Hello from React");
     // this render tag is responsible to take this object and convert into heading tag and put into DOM
 
     // {} this is basically a object where you give attribute in that
+    // tilde(~) and caret(^)
 
+// suppose there is minor upgrade then we use 
+// for majoe version we used tilde
     // Create Nested Element
+    // Bundlers ,webpack,parcel it packages our app so we can push to in our productions
+
+    // parcel will give strength in our app 
+    //parcel is a beast
+    // Dev Dependencies and Normal Dependencies
+    //npm is a package manager its biggest package all the package libraries all comes from npm
+
+    // package.json is basically configuration of npm files
+    // packagelock.json keep lock record of exact version of dependencies
+    // node modules kind of database which contains all the dependencies we install only parcel but parcel has own dependencies so this
+    // is called Transitive dependencies
     const parent=React.createElement("div",{id:"parent"}
     ,[
         React.createElement("div",{id:"child1"}
-    ,[React.createElement("h1",{},"H1 Tag"),
-    React.createElement("h1",{},"H2 Tag")]),
+    ,[React.createElement("h1",{}," This is H1 rrTag"),
+    React.createElement("h1",{},"This isH2 Tag")]),
 
     React.createElement("div",{id:"child2"}
-    ,[React.createElement("h1",{},"H1 Tag"),
-    React.createElement("h1",{},"H2 Tag")])
+    ,[React.createElement("h1",{},"That is H1 Tag"),
+    React.createElement("h1",{},"That is H2 Tag")])
 ]);
     const root=ReactDOM.createRoot(document.getElementById("root"));
     root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Welcom to React 
+# Welcome to React 
+
+# Parcel
+-It is doing dev build serever for us
+-Local Server
+-Refreshinf fast HMR Hot Module Replacement
+-File Watching Algorithm C++ change anything as Build in 4msIt is reducing the time actually because -Parcel is caching this for us
+-if we delete parcel cache it will take 456 ms random
+Image Optimization
+Hinification
+Bundling
+Compress our file
+Consistent Hashing
+Code Splitting
+Differential Bundling our app may be open in different oldr browser
+HTTPS 
+Hot Reloding
+Diagonist
+Reliable Caching
+Tree shaking remove unnecessary code
+Different Dev and Prod bundles
+
+
+
